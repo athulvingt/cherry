1dee18a671cb1d874541116f868103879ebcaf7f
athul <athul@ivasystems.com>
2024-10-22 21:29:32 +0530

    initial commit

---
+++ lib/yolo_service.dart
@@ -0,0 +1,9 @@
+// import 'package:ultralytics_yolo/ultralytics_yolo.dart';
+
+// final model = LocalYoloModel(
+//   id: 'model-id',
+//   task: Task.detect, // or Task.classify
+//   format: Format.tflite, // or Format.coreml for iOS
+//   modelPath: 'assets/model.tflite', // Update with the correct path
+//   metadataPath: 'assets/model.metadata', // Optional if you have metadata
+// );
